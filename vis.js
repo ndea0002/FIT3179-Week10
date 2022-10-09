0877b3503a2ab696fa965a5e667f6162ea50a8eb
ndea0002 <113740883+ndea0002@users.noreply.github.com>
2022-10-09 13:19:02 +0530

    Adding the js file

---
+++ vis.js
@@ -0,0 +1,11 @@
+var vg_1 = "JS/worldmap.vg.json";
+
+vegaEmbed("#chart_1", vg_1).then(function(result) {
+
+   }).catch(console.error);
+
+var vg_2 = "JS/bubble.vg.json";
+
+   vegaEmbed("#chart_2", vg_2).then(function(result) {
+   
+      }).catch(console.error);
